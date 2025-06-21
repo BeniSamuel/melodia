640f89b2c2ef8693f41d3e1e37abfb03cbd0745f
benisamuel <benisamuel566@gmail.com>
2025-06-21 09:20:16 +0200

    added playlist page

---
+++ src/pages/playlist/Playlist.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeLayout from "../../layouts/common/HomeLayout";
+
+const Playlist: React.FC = () => {
+  return (
+    <HomeLayout>
+        <div></div>
+    </HomeLayout>
+  );
+};
+
+export default Playlist;
