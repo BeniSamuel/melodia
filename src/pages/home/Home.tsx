20f0848caec57a1bdae65f12b6be500923049f7a
benisamuel <benisamuel566@gmail.com>
2025-06-21 11:30:54 +0200

    Improved the home page ui

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import HomeLayout from "../../layouts/common/HomeLayout";
 import PopularSong from "../../components/home/PopularSong";
 
 const Home: React.FC = () => {
-  const [showLeftComponent, setShowLeftComponent] = useState(false);
+
   return (
     <HomeLayout>
       <div className=" flex flex-col py-5 px-5">
