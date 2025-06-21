264b0e188c99ffb158c5c4c305c40cadc3bedd2c
benisamuel <benisamuel566@gmail.com>
2025-06-21 09:18:08 +0200

    added app data file for storing most commonly used data

---
+++ src/data/app.data.ts
@@ -0,0 +1,93 @@
+import { LikeType } from "../types/likes.type";
+import { SongType } from "../types/song.type";
+
+export const likes: LikeType[] = [
+  {
+    id: 1,
+    cover: "",
+    title: "OnlyMe#",
+    songCount: 8,
+  },
+  {
+    id: 1,
+    cover: "",
+    title: "#IWG",
+    songCount: 8,
+  },
+  {
+    id: 1,
+    cover: "",
+    title: "#Shit2kAges",
+    songCount: 8,
+  },
+  {
+    id: 1,
+    cover: "",
+    title: "#CRG",
+    songCount: 8,
+  },
+  {
+    id: 1,
+    cover: "",
+    title: "#OnTheWay",
+    songCount: 8,
+  },
+  {
+    id: 1,
+    cover: "",
+    title: "TypeShit#",
+    songCount: 8,
+  },
+];
+
+export const songs: SongType[] = [
+  {
+    id: 1,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 2,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 3,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 4,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 5,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 6,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 7,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  {
+    id: 8,
+    song_name: "iyo foto",
+    artist: "Bruce melodie",
+    cover: "blue"
+  },
+  
+]
