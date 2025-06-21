0f5f889d4798c89d04418181534c09f9270f634e
benisamuel <benisamuel566@gmail.com>
2025-06-21 11:30:22 +0200

    improved home layout to support home-right-component

--- src/layouts/common/HomeLayout.tsx
+++ src/layouts/common/HomeLayout.tsx
@@ -3,8 +3,12 @@ import Navbar from "../../components/common/navbar/Navbar";
 import HomeLeftCard from "../../components/common/cards/HomeLeftCard";
 import SongsCard from "../../components/common/cards/SongsCard";
 import { SongCardType } from "../../types/song-card.type";
+import { usePlayer } from "../../context/PlayContext";
+import HomeRightCard from "../../components/common/cards/HomeRightCard";
 
 const HomeLayout: React.FC<SongCardType> = ({ children }) => {
+  const { isPlaying } = usePlayer();
+
   return (
     <div className=" bg-[#000000] h-[100vh] py-[0.35rem] px-[0.35rem] flex flex-col gap-3">
       <div>
@@ -13,6 +17,7 @@ const HomeLayout: React.FC<SongCardType> = ({ children }) => {
       <div className=" flex flex-row gap-3">
         <HomeLeftCard />
         <SongsCard>{children}</SongsCard>
+        {isPlaying && <HomeRightCard />}
       </div>
     </div>
   );
