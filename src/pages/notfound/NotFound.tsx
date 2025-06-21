ea32305574673f777e7c97bd0b25071e55594dcc
benisamuel <benisamuel566@gmail.com>
2025-06-21 09:19:48 +0200

    added not found page

---
+++ src/pages/notfound/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className=" flex flex-col items-center justify-center bg-[#131313] h-[100vh]">
+      <div className=" text-white">Sorry page not found</div>
+    </div>
+  );
+};
+
+export default NotFound;
