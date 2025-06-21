b345a58f6fc273f4c400b4ac39d738bc8291ae00
benisamuel <benisamuel566@gmail.com>
2025-06-21 20:23:28 +0200

    improved song-indicator

--- src/components/common/cards/SongIndicator.tsx
+++ src/components/common/cards/SongIndicator.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { usePlayer } from "../../../context/PlayContext";
+import AudioPlayer from "../card/AudioPlayer";
 
 const SongIndicator: React.FC = () => {
-  return (
-    <div className=' text-white'>SongIndicator</div>
-  )
-}
+  const { currentSong } = usePlayer();
 
-export default SongIndicator
+  return (
+    <div className=" text-white px-3 flex flex-row justify-between items-center">
+      <div className=" flex flex-row gap-4 items-center ">
+        <div className=" bg-gradient-to-b from-[#225673] to-[#40A2D9] h-16 w-16 rounded-xl" />
+        <div>
+          <div>{currentSong?.song_name}</div>
+          <div className=" text-[#686868]">{currentSong?.artist}</div>
+        </div>
+      </div>
+      <div>
+        <AudioPlayer />
+      </div>
+    </div>
+  );
+};
+
+export default SongIndicator;
