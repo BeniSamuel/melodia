79117e5b2cd798beb0aeb94944c3abee00b34f43
benisamuel <benisamuel566@gmail.com>
2025-06-22 15:52:14 +0200

    added the audio page

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import Home from "./pages/home/Home";
 import NotFound from "./pages/notfound/NotFound";
 import Playlist from "./pages/playlist/Playlist";
 import PlayProvider from "./context/PlayContext";
+import Audio from "./pages/audio/Audio";
 
 const App: React.FC = () => {
   return (
     <PlayProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route path="/" element={<Welcome />} />  
           <Route path="/home" element={<Home />} />
           <Route path="/playlist" element={<Playlist />} />
+          <Route path="/audio/:id" element={<Audio />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
