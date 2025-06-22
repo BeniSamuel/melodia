b77db5ee531ce5363ff5b22d5ab4a16f592698d5
benisamuel <benisamuel566@gmail.com>
2025-06-22 16:21:30 +0200

    added an overflow on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import HomeLayout from "../../layouts/common/HomeLayout";
 import PopularSong from "../../components/home/PopularSong";
+import RecentlyPlayed from "../../components/home/RecentlyPlayed";
 
 const Home: React.FC = () => {
 
   return (
     <HomeLayout>
-      <div className=" flex flex-col py-5 px-5">
+      <div className=" flex flex-col py-5 px-5 gap-16">
         <PopularSong />
+        <RecentlyPlayed />
       </div>
     </HomeLayout>
   );
