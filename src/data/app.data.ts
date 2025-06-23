9d7feda00b12737f28078b53c5c76d16477b899e
benisamuel <benisamuel566@gmail.com>
2025-06-23 20:06:50 +0200

    added improved layout in app.data

--- src/data/app.data.ts
+++ src/data/app.data.ts
@@ -45,49 +45,48 @@ export const songs: SongType[] = [
     id: 1,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 2,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 3,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 4,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 5,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 6,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 7,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
   {
     id: 8,
     song_name: "iyo foto",
     artist: "Bruce melodie",
-    cover: "blue"
+    cover: "blue",
   },
-  
-]
+];
